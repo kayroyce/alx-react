0d61cfdf171ffc91cb150dd9b6251a23a3e9d9ac
kayroyce <kayroyce2005@gmail.com>
2023-02-25 11:06:20 +0100

    node_modules

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+	entry: './js/dashboard_main.js',
+	output: {
+		path: path.resolve(__dirname, 'public'),
+		filename: 'bundle.js',
+	},
+	mode: 'production',
+	module: {
+		rules: [
+			{ 
+				test: /\.css$/i, 
+				use: ['raw-loader', 'style-loader'], },
+			{ 
+				test: /\.(gif|png|jpe?g|svg)$/i, 
+				use: [
+					'file-loader', 
+					{
+						loader: 'image-webpack-loader',
+						options: {bypassingOnDebug: true, disable: true,},
+					}]
+			},
+		],
+	},
+};
